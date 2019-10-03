c551001c86d2bb9ce2395156887b8640a22517d4
Fandorin <SherJlok15@gmail.com>
2019-10-03 09:36:26 +0300

    Updates

---
+++ precache-manifest.9308417bcef161ef5da9bb7347928b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55b118257014173bfbd9894537abd43",
+    "url": "/buki_test_v2.1/index.html"
+  },
+  {
+    "revision": "ccf80d0cce9315a6a7f2",
+    "url": "/buki_test_v2.1/static/css/main.03d3307a.chunk.css"
+  },
+  {
+    "revision": "dad58e832ffc4fdc10a0",
+    "url": "/buki_test_v2.1/static/js/2.36045dc0.chunk.js"
+  },
+  {
+    "revision": "ccf80d0cce9315a6a7f2",
+    "url": "/buki_test_v2.1/static/js/main.fa3f2ef1.chunk.js"
+  },
+  {
+    "revision": "5ea347525a8b89f06a2e",
+    "url": "/buki_test_v2.1/static/js/runtime-main.fa592a8f.js"
+  },
+  {
+    "revision": "07d53b5b4d43b671764d7075be494019",
+    "url": "/buki_test_v2.1/static/media/Arial.07d53b5b.ttf"
+  }
+]);
